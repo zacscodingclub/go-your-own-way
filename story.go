f0ec6cc765f371ee14a9726558605a12f888209d
Zac Baston <zbaston@gmail.com>
2017-12-12 12:47:30 -0800

    allow for custom templates and fallback to default

--- story.go
+++ story.go
@@ -35,30 +35,76 @@ var defaultTemplate = `
 		<title>Go Your Own Way</title>
 	</head>
 	<body>
-		<h1>{{.Title}}</h1>
+		<section class="page">
+			<h1>{{.Title}}</h1>
 
-		{{range .Paragraphs}}
-			<p>{{.}}</p>
-		{{end}}
+			{{range .Paragraphs}}
+				<p>{{.}}</p>
+			{{end}}
 
-		<ul>
-		{{range .Options}}
-			<li><a href="/{{.Chapter}}">{{.Text}}</a></li>
-		{{end}}
-		</ul>
+			<ul>
+			{{range .Options}}
+				<li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+			{{end}}
+			</ul>
+		</section>
 	</body>
+	<style>
+		body {
+			font-family: helvetica, arial;
+		}
+		h1 {
+			text-align:center;
+			position:relative;
+		}
+		.page {
+			width: 80%;
+			max-width: 500px;
+			margin: auto;
+			margin-top: 40px;
+			margin-bottom: 40px;
+			padding: 80px;
+			background: #FFFCF6;
+			border: 1px solid #eee;
+			box-shadow: 0 10px 6px -6px #777;
+		}
+		ul {
+			border-top: 1px dotted #ccc;
+			padding: 10px 0 0 0;
+			-webkit-padding-start: 0;
+		}
+		li {
+			padding-top: 10px;
+		}
+		a,
+		a:visited {
+			text-decoration: none;
+			color: #6295b5;
+		}
+		a:active,
+		a:hover {
+			color: #7792a2;
+		}
+		p {
+			text-indent: 1em;
+		}
+	</style>
 </html>`
 
 func init() {
 	tpl = template.Must(template.New("").Parse(defaultTemplate))
 }
 
-func NewHandler(s Story) http.Handler {
-	return handler{s}
+func NewHandler(s Story, t *template.Template) http.Handler {
+	if t == nil {
+		t = tpl
+	}
+	return handler{s, t}
 }
 
 type handler struct {
 	s Story
+	t *template.Template
 }
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
