36581f5171bd78e1a35ed60dbeaa1d7e87564ab9
Zac Baston <zbaston@gmail.com>
2017-12-12 13:15:21 -0800

    include servemux for more dynamic paths

--- cmd/gyow-web/main.go
+++ cmd/gyow-web/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/zacscodingclub/go-your-own-way"
 )
@@ -21,11 +23,14 @@ func main() {
 
 	story, err := gyow.JsonStory(f)
 	errorAndExit(err)
+	tpl := template.Must(template.New("").Parse(storyTemplate))
+	h := gyow.NewHandler(story, gyow.WithTemplate(tpl), gyow.WithPathFunc(pathFn))
 
-	h := gyow.NewHandler(story)
+	mux := http.NewServeMux()
+	mux.Handle("/story/", h)
+
 	fmt.Printf("Starting server at port: %d\n", *port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
-
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), mux))
 }
 
 func errorAndExit(e error) {
@@ -34,3 +39,78 @@ func errorAndExit(e error) {
 		os.Exit(1)
 	}
 }
+
+func pathFn(r *http.Request) string {
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "/story" || path == "/story/" {
+		path = "/story/intro"
+	}
+
+	return path[len("/story/"):]
+}
+
+var storyTemplate = `
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>Go Your Own Way</title>
+	</head>
+	<body>
+		<section class="page">
+			<h1>{{.Title}}</h1>
+
+			{{range .Paragraphs}}
+				<p>{{.}}</p>
+			{{end}}
+
+			<ul>
+			{{range .Options}}
+				<li><a href="/story/{{.Chapter}}">{{.Text}}</a></li>
+			{{end}}
+			</ul>
+		</section>
+	</body>
+	<style>
+		body {
+			font-family: helvetica, arial;
+		}
+		h1 {
+			text-align:center;
+			position:relative;
+		}
+		.page {
+			width: 80%;
+			max-width: 500px;
+			margin: auto;
+			margin-top: 40px;
+			margin-bottom: 40px;
+			padding: 80px;
+			background: #FFFCF6;
+			border: 1px solid #eee;
+			box-shadow: 0 10px 6px -6px #777;
+		}
+		ul {
+			border-top: 1px dotted #ccc;
+			padding: 10px 0 0 0;
+			-webkit-padding-start: 0;
+		}
+		li {
+			padding-top: 10px;
+		}
+		a,
+		a:visited {
+			text-decoration: none;
+			color: #6295b5;
+		}
+		a:active,
+		a:hover {
+			color: #7792a2;
+		}
+		p {
+			text-indent: 1em;
+		}
+	</style>
+</html>`
--- story.go
+++ story.go
@@ -25,6 +25,14 @@ type Option struct {
 	Chapter string `json:"arc"`
 }
 
+type handler struct {
+	s      Story
+	t      *template.Template
+	pathFn func(r *http.Request) string
+}
+
+type HandlerOption func(h *handler)
+
 var defaultTemplate = `
 <!DOCTYPE html>
 	<html lang="en">
@@ -95,28 +103,40 @@ func init() {
 	tpl = template.Must(template.New("").Parse(defaultTemplate))
 }
 
-func NewHandler(s Story, t *template.Template) http.Handler {
-	if t == nil {
-		t = tpl
+func WithTemplate(t *template.Template) HandlerOption {
+	return func(h *handler) {
+		h.t = t
 	}
-	return handler{s, t}
 }
 
-type handler struct {
-	s Story
-	t *template.Template
+func WithPathFunc(fn func(r *http.Request) string) HandlerOption {
+	return func(h *handler) {
+		h.pathFn = fn
+	}
 }
 
-func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func defaultPathFn(r *http.Request) string {
 	path := strings.TrimSpace(r.URL.Path)
 	if path == "" || path == "/" {
 		path = "/intro"
 	}
 
-	path = path[1:]
+	return path[1:]
+}
 
+func NewHandler(s Story, opts ...HandlerOption) http.Handler {
+	h := handler{s, tpl, defaultPathFn}
+	for _, opt := range opts {
+		opt(&h)
+	}
+
+	return h
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := h.pathFn(r)
 	if chapter, ok := h.s[path]; ok {
-		err := tpl.Execute(w, chapter)
+		err := h.t.Execute(w, chapter)
 		if err != nil {
 			log.Printf("%v", err)
 			http.Error(w, "Something went wrong...", http.StatusInternalServerError)
